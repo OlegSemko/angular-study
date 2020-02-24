45df087977099ff72c15c6b146d0b6c9e5796cdd
Oleg Semko <semoasem@gmail.com>
2020-02-24 10:20:46 +0200

    pass event to grandpa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,42 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  animal: string;
+  ids: number[] = [1,2,3,4,5];
+  constructor(private router: Router, private route: ActivatedRoute) {}
   name = 'angularbase';
   in: string = 'hello'; 
+  // onClick(elem) {
+  //   console.log("from app")
+  //   this.router.navigate([], {
+  //     relativeTo: this.route, queryParams:{
+  //       [this.name]: this.ids.join(',') || null
+  //     },
+  //     queryParamsHandling: 'merge'
+  //   })
+  // }
 
+  onClicked(number: any) {
+    console.log("app", number);
+      this.router.navigate([], {
+      relativeTo: this.route, queryParams:{
+        [this.name]: number
+        // this.ids.join(',') || null
+      },
+      queryParamsHandling: 'merge'
+    })
+    console.log(this.route.snapshot.queryParamMap.get("angularbase"));
+
+  }
+
+
+  ngOnInit() {
+    console.log(this.route.snapshot.queryParamMap.get("angularbase"));
+  }
 }
--- src/app/wife/wife.component.ts
+++ src/app/wife/wife.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-wife',
@@ -6,7 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wife.component.scss']
 })
 export class WifeComponent implements OnInit {
+  @Output() onClicked = new EventEmitter();
+  onClick(number: any) {
+    console.log('mum', number);
+    this.onClicked.emit(number);
+
+  }
+
+  items: number[] = [1,2,3,4,5,6];
+
   name='mum'
+  sun="blue car"
+  doughter="pink doll"
   constructor() { }
 
   ngOnInit() {
